0859ac56739baa92657ca808aa6099010ec34519
Newton Lomar <newtonlomar16@gmail.com>
2022-12-08 16:41:23 -0300

    adding rectangle functions to external module

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+var rect = require("./rectangle");
 
 function solveRect(l, b) {
   console.log(`Solving for rectangle with l = ${l} and b = ${b}`);
@@ -14,6 +11,8 @@ function solveRect(l, b) {
     console.log(`The area of the rectangle is ${rect.area(l, b)}`);
     console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`);
   }
+
+  console.log();
 }
 
 solveRect(2, 4);
